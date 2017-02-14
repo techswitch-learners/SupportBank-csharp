bbfdd3ecfc03a8b15f131f80f6a9980a56f7cc22
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-14 14:15:16 +0000

    Sample data and project framework

---
+++ README.md
@@ -0,0 +1,8 @@
+# SupportBank
+
+This repository contains:
+
+* Some sample data for the SupportBank challenge
+* A sample solution
+
+Copyright © 2017 Softwire - All Rights Reserved
---
+++ SupportBank.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SupportBank.Console
+{
+  class Program
+  {
+    static void Main()
+    {
+      // Please try to solve the problem yourself before looking at the model answer.
+      // Your trainer will work through the interesting bits of that solution in due course.
+    }
+  }
+}
